20e927ebb35f633e2b24baafd1aa5c165a45b377
j4rl <charlie.jarl@gmail.com>
2022-02-08 10:01:23 +0100

    ...

--- app.js
+++ app.js
@@ -45,8 +45,6 @@ document.addEventListener("DOMContentLoaded", function() {
     function dragStart() {
         colorBeingDragged = this.style.background;
         squareIdBeingDragged = parseInt(this.id);
-        console.log(squareIdBeingDragged + " is being dragged");
-        console.log(colorBeingDragged + " color being dragged");
     }
 
     function dragEnd() {
@@ -65,23 +63,6 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             squares[squareIdBeingDragged].style.background = colorBeingDragged;
         }
-    }
-
-    function moveDown() {
-        //const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
-        for (i = width; i < (width * width); i++) {
-            if (squares[i].style.background === '') {
-                squares[i].style.background = squares[i - width].style.background;
-                squares[i - width].style.background = '';
-            }
-        }
-        for (i = 0; i < width; i++) {
-            if (squares[i].style.background === '') {
-                let randomColor = Math.floor(Math.random() * candyColors.length); //Make a random number between 0 and whatever size the color array is
-                squares[i].style.background = candyColors[randomColor];
-            }
-        }
-
     }
 
     function dragOver(e) {
@@ -103,46 +84,61 @@ document.addEventListener("DOMContentLoaded", function() {
         squares[squareIdBeingDragged].style.background = colorBeingReplaced;
     }
 
-    function checkRowForThree() {
-        for (i = 0; i < 62; i++) {
-            let rowOfThree = [i, i + 1, i + 2];
-            let decidedColor = squares[i].style.background;
-            const isBlank = squares[i].style.background === '';
-            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55];
 
-            if (notValid.includes(i)) continue //This is pretty much a GOTO
+    // function checkRowForThree() {
+    //     for (i = 0; i < 62; i++) {
+    //         let rowOfThree = [i, i + 1, i + 2];
+    //         let decidedColor = squares[i].style.background;
+    //         const isBlank = squares[i].style.background === '';
+    //         const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55];
 
-            if (rowOfThree.every(index => squares[index].style.background === decidedColor && !isBlank)) {
-                score += 3;
-                document.getElementById('score').innerHTML = score;
-                rowOfThree.forEach(index => {
-                    squares[index].style.background = '';
-                })
-            }
-        }
-    }
+    //         if (notValid.includes(i)) continue //This is pretty much a GOTO
+
+    //         if (rowOfThree.every(index => squares[index].style.background === decidedColor && !isBlank)) {
+    //             score += 3;
+    //             document.getElementById('score').innerHTML = score;
+    //             rowOfThree.forEach(index => {
+    //                 squares[index].style.background = '';
+    //             })
+    //         }
+    //     }
+    // }
     //checkRowForThree();
 
-    function checkColumnForThree() {
-        for (i = 0; i < 48; i++) {
-            let columnOfThree = [i, i + width, i + width + width];
-            let decidedColor = squares[i].style.background;
-            const isBlank = squares[i].style.background === '';
-            //const notValid=[];
-            //if(notValid.includes(i)) continue;
-            if (columnOfThree.every(index => squares[index].style.background === decidedColor && !isBlank)) {
-                score += 3;
-                document.getElementById('score').innerHTML = score;
-                columnOfThree.forEach(index => {
-                    squares[index].style.background = '';
-                })
+    // function checkColumnForThree() {
+    //     for (i = 0; i < 48; i++) {
+    //         let columnOfThree = [i, i + width, i + width + width];
+    //         let decidedColor = squares[i].style.background;
+    //         const isBlank = squares[i].style.background === '';
+    //         //const notValid=[];
+    //         //if(notValid.includes(i)) continue;
+    //         if (columnOfThree.every(index => squares[index].style.background === decidedColor && !isBlank)) {
+    //             score += 3;
+    //             document.getElementById('score').innerHTML = score;
+    //             columnOfThree.forEach(index => {
+    //                 squares[index].style.background = '';
+    //             })
+    //         }
+    //     }
+    // }
+    //checkColumnForThree();
+    function moveDown() {
+        for (i = width; i < (width * width); i++) {
+            if (squares[i].style.background === '') {
+                squares[i].style.background = squares[i - width].style.background;
+                squares[i - width].style.background = '';
             }
         }
+        for (i = 0; i < width; i++) {
+            if (squares[i].style.background === '') {
+                let randomColor = Math.floor(Math.random() * candyColors.length);
+                squares[i].style.background = candyColors[randomColor];
+            }
+        }
+
     }
-    //checkColumnForThree();
+
     function checkMove(checkCase, points) {
-
-
 
         for (i = 0; i < width * width; i++) {
             switch (checkCase) {
@@ -151,7 +147,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     arrNoGo = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
                     break;
                 case 'threeColumn':
-                    arrCase = [i, i + width, i + width + width];
+                    arrCase = [i, i + width, i + width * 2];
                     arrNoGo = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]; // code block                // code block
                     break;
                 case 'fourRow':
@@ -159,7 +155,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     arrNoGo = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 61, 62, 63]; // code block
                     break;
                 case 'fourColumn':
-                    arrCase = [i, i + width, i + width + width, i + width + width + width];
+                    arrCase = [i, i + width, i + width * 2, i + width * 3];
                     arrNoGo = [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]; // code block
                     break;
                 case 'fiveRow':
@@ -167,7 +163,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     arrNoGo = [4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31, 36, 37, 38, 39, 44, 45, 46, 47, 52, 53, 54, 55, 60, 61, 62, 63]; // code block
                     break;
                 case 'fiveColumn':
-                    arrCase = [i, i + width, i + width + width, i + width + width + width + width];
+                    arrCase = [i, i + width, i + width * 2, i + width * 3, i + width * 4];
                     arrNoGo = [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]; // code block
                     break;
                 case 'pool':
@@ -175,7 +171,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     arrNoGo = [55, 56, 57, 58, 59, 60, 61, 62, 63]; // code block
                     break;
                 case 'cross':
-                    arrCase = [i, i + width - 1, i + width + 1, i + width + width];
+                    arrCase = [i, i + width - 1, i + width + 1, i + width * 2];
                     arrNoGo = [47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]; // code block
                     break;
                 default:
@@ -202,10 +198,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
         checkMove('pool', 50);
         checkMove('cross', 100);
-        checkMove('fourRow', 5);
-        checkMove('fourColumn', 5);
         checkMove('fiveRow', 10);
         checkMove('fiveColumn', 10);
+
+        checkMove('fourRow', 5);
+        checkMove('fourColumn', 5);
+
         checkMove('threeRow', 3);
         checkMove('threeColumn', 3);
         moveDown();
